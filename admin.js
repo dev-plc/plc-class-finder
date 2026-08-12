c09d068eba1129965b9020f317a90bdf75cee15e
practical1948-jpg <practical1948@gmail.com>
2026-08-12 13:06:28 +0900

    chore: 프로젝트 모듈 캐시 버전 v45로 업데이트

--- admin.js
+++ admin.js
@@ -10,8 +10,8 @@ import {
     updateAttendanceBatch,
     getCohortId,
     subscribe,
-} from './scripts/members-data.js?v=44';
-import { matches as hangulMatches } from './scripts/hangul.js?v=44';
+} from './scripts/members-data.js?v=45';
+import { matches as hangulMatches } from './scripts/hangul.js?v=45';
 
 // 로그인 확인
 if (!sessionStorage.getItem('adminLoggedIn')) {
